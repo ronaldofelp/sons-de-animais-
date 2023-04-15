7070c249a413680aba0d2791ad6ed9efe1624684
ronaldofelp <ronaldoamorim144@gmaail.com>
2023-04-15 13:40:36 -0300

    primeiro commit

---
+++ js/mais.js
@@ -0,0 +1,10 @@
+function tocaSom() {
+    document.querySelector("#som_animal_vaca").play()
+
+}
+
+const tecla = document.querySelector(".Imagem_vaca")
+
+tecla.onclick = function () {
+    tocaSom()
+}
