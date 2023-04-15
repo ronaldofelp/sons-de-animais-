fd39e02fc04e28cd3a7533555ea72820495b0f7a
ronaldofelp <ronaldoamorim144@gmaail.com>
2023-04-15 17:15:34 -0300

    Design e funcionalidades prontos, mas falta adicionar os demais audios

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 function tocaSom() {
-    document.querySelector("#som_animal_vaca").play()
-
+    document.querySelector("#som_animal_vaca").play();
 }
 
-const listaDeTeclas = document.querySelectorAll(".imagem")
-
-
+const listaDeTeclas = document.querySelectorAll(".imagem");
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
 
-    const tecla = listaDeTeclas[i]
-    tecla.onclick = function () {
-        tocaSom()
-    }
+    tecla.onclick = function() {
+        tocaSom();
+    };
 
+    tecla.addEventListener('click', function() {
+        
+        this.classList.add('ativo');
 
-
-
-
+        
+        for (let j = 0; j < listaDeTeclas.length; j++) {
+            if (listaDeTeclas[j] !== this) {
+                listaDeTeclas[j].classList.remove('ativo');
+            }
+        }
+    });
 }
